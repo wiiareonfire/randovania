cb91b3742e5eea2097605b960b2103595f8973a1
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2023-09-11 11:20:55 +0300

    Fixing ruff errors without autofix

--- randovania/games/am2r/exporter/joke_hints.py
+++ randovania/games/am2r/exporter/joke_hints.py
@@ -41,8 +41,8 @@ JOKE_HINTS = [
     ),
     (
         "*Ancient tongue, non-decipherable. The contents in full:#"
-        "ＮＡＧＧＩＮＧＳ　ＦＲＯＭ　ＰＡＳＴ ＳＯ#"
-        "ＮＯＷ　ＩＭ　ＨＥＬＰＩＮＧ　ＶＩＡ　ＤＥＶ#"
-        "ＡＬＡＳ　ＧＬＡＤ　ＦＯＲ　ＹＥ*"
+        "ＮＡＧＧＩＮＧＳ　ＦＲＯＭ　ＰＡＳＴ ＳＯ#"  # noqa: RUF001
+        "ＮＯＷ　ＩＭ　ＨＥＬＰＩＮＧ　ＶＩＡ　ＤＥＶ#"  # noqa: RUF001
+        "ＡＬＡＳ　ＧＬＡＤ　ＦＯＲ　ＹＥ*"  # noqa: RUF001
     ),
 ]
